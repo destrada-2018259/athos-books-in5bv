7b1e7e3f80fb2af5b0ace0d91967386e41a9b7de
Daniel Estrada <destrada-2018259@kinal.edu.gt>
2022-08-20 12:26:27 -0600

    Primer commit

---
+++ src/main/java/gt/org/danielestrada/athos/books/in5bv/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package gt.org.danielestrada.athos.books.in5bv;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
